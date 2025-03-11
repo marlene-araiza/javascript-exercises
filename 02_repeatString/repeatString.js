8bdcc8cbeec47fb2c022d855ba5497b0a90efbce
marlene-araiza <marlene_araiza@outlook.com>
2025-03-11 00:44:09 -0700

    Complete 02_repeatString.js exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,12 @@
-const repeatString = function(word, num) {
-    for (let i = 0; i < num; i++) {
-        return word;
-    }  
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    let string = ""; //declare a variable with value of "" no spaces in order to join the words given
+    for (let i = 0; i < times; i++) {
+        string += word; // string = string("") + word
+    }
+    return string;
 };
 // Do not edit below this line
 module.exports = repeatString;
