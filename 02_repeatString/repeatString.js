ccd8b0b1629c1731d343f7af86ce13172a000b7c
marlene-araiza <marlene_araiza@outlook.com>
2025-03-11 00:25:54 -0700

    Complete 03_reverseString.js exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    for (let i = 0; i < num; i++) {
+        return word;
+    }  
 };
-
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    return text.split('').reverse().join(''); //.split() turns string into an Array , reverse() shows the Array backwards, join() is how we want join the array elements
 };
 
 // Do not edit below this line
