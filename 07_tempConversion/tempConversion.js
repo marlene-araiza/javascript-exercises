6b09fc6faabd4179bbc10fc958566cf8e4a83b40
marlene-araiza <marlene_araiza@outlook.com>
2025-03-11 21:34:27 -0700

    Complete 07_tempConversion.js exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInCelsius) {
+ c = (temperatureInCelsius - 32) * (5/9);
+ cRounded = Math.round(c * 10) / 10;
+ return cRounded; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInFahreinheit) { 
+  f = (temperatureInFahreinheit * (9/5) + 32);
+  fRounded = Math.round(f * 10) / 10;
+  return fRounded;
 };
 
 // Do not edit below this line
